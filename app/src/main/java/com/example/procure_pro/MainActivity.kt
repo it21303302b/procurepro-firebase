36259c0f253ff706b9c0fb96ec5476153c0e50ed
bimsara weerakoon <99312076+it21303302b@users.noreply.github.com>
2023-10-16 01:03:19 +0530

    login fixed

--- app/src/main/java/com/example/procure_pro/MainActivity.kt
+++ app/src/main/java/com/example/procure_pro/MainActivity.kt
@@ -4,16 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.procure_pro.databinding.ActivityMainBinding
+import io.realm.Realm
+import io.realm.mongodb.Credentials
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+import org.bson.Document
 
 class MainActivity : AppCompatActivity() {
+    lateinit var app: App
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Realm.init(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding?.root
+        setContentView(view)
         supportActionBar?.hide()
+
+        val appId = "devicesync-agtgn" // Replace with your actual Realm app ID
+
+        val config = AppConfiguration.Builder(appId)
+            .build()
+
+        app = App(config)
     }
 
-    fun log(view: View){
-        val dashIntent = Intent(this,Dashboard::class.java)
-        startActivity(dashIntent)
+    fun log(view: View) {
+        val username = binding?.etsitemgrName?.text.toString()
+        val password = binding?.etEnterPassword?.text.toString()
+
+        // Create a Document with username and password fields1
+        val payload = Document("username", username).append("password", password)
+
+        val credentials = Credentials.customFunction(payload)
+
+        app.loginAsync(credentials) { user ->
+            if (user.isSuccess) {
+                // Successfully logged in
+                val loggedInUser = user.get()
+                val dashIntent = Intent(this, Dashboard::class.java)
+                startActivity(dashIntent)
+            } else {
+                // Failed to login, handle the error
+                val error = user.getError()
+                Toast.makeText(this, error?.localizedMessage ?: "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 }
